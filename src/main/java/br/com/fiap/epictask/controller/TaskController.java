f3fb58f33ce43d249e7903014be7060fe3d8100c
joaocarlos <joaocarlos@pop-os.localdomain>
2021-08-17 16:30:04 -0300

    :sparkles: task list and validation

--- src/main/java/br/com/fiap/epictask/controller/TaskController.java
+++ src/main/java/br/com/fiap/epictask/controller/TaskController.java
@@ -1,10 +1,16 @@
 package br.com.fiap.epictask.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.epictask.model.Task;
 import br.com.fiap.epictask.repository.TaskRepository;
@@ -16,18 +22,22 @@ public class TaskController {
 	private TaskRepository repository;
 	
 	@GetMapping("/task")
-	public String index() {
-		return "tasks";
+	public ModelAndView index() {
+		List<Task> tasks = repository.findAll();
+		ModelAndView modelAndView = new ModelAndView("tasks");
+		modelAndView.addObject("tasks", tasks);
+		return modelAndView;
 	}
 	
 	@PostMapping("/task")
-	public String save(Task task) {
+	public String save(@Valid Task task, BindingResult result) {
+		if (result.hasErrors()) return "task-form";
 		repository.save(task);
 		return "tasks";
 	}
 	
 	@RequestMapping("/task/new")
-	public String create() {
+	public String create(Task task) {
 		return "task-form";
 	}
 
--- src/main/java/br/com/fiap/epictask/model/Task.java
+++ src/main/java/br/com/fiap/epictask/model/Task.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -14,8 +18,15 @@ public class Task {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "O título é obrigatório. Digite um título.")
 	private String title;
+	
+	@Size(min = 15, message = "A descrição deve ter pelo menos 15 caracteres")
 	private String description;
+	
+	@Min(value = 10, message = "A pontuação mínima é 10")
+	@Max(value = 500, message = "A pontuação máxima é 500")
 	private int points;
 
 }
